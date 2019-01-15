1fe6d67518c608e9a05c2f76f66fa226a7e88553
Oleksandr Chekhovskyi <oleksandr.chekhovskyi@favro.com>
2019-01-15 10:37:45 +0100

    Fix source maps support for legacy compile handlers.

--- tools/isobuild/compiler-plugin.js
+++ tools/isobuild/compiler-plugin.js
@@ -592,6 +592,8 @@ class ResourceSlot {
   // original data. #HardcodeJs
   _addDirectlyToJsOutputResources() {
     this.addJavaScript({
+      bare: this.inputResource.bare,
+      sourceMap: this.inputResource.sourceMap,
       ...(this.inputResource.fileOptions || {}),
       path: this.inputResource.path,
       data: this.inputResource.data,
