d7e680ddc10ed3333651fe9fea3858c0357b2a52
Oleksandr Chekhovskyi <oleksandr.chekhovskyi@favro.com>
2019-08-07 12:18:48 +0300

    Generate browserId on the client and pass it to the server on login

--- packages/accounts-password/password_server.js
+++ packages/accounts-password/password_server.js
@@ -167,11 +167,11 @@ Accounts.registerLoginHandler("password", options => {
   if (!options.password)
     return undefined; // don't handle
 
-  check(options, {
+  check(options, Match.ObjectIncluding({
     user: Accounts._userQueryValidator,
     password: passwordValidator,
     code: Match.Optional(NonEmptyString),
-  });
+  }));
 
 
   const user = Accounts._findUserByQuery(options.user, {fields: {
