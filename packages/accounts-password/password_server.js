30f36c980d10747af14c3ed8afc0c1bf4c068435
Oleksandr Chekhovskyi <oleksandr.chekhovskyi@favro.com>
2019-08-27 15:35:41 +0300

    Send browserId also when calling createUser, verifyEmail and resetPassword methods

--- packages/accounts-password/password_server.js
+++ packages/accounts-password/password_server.js
@@ -705,6 +705,7 @@ Accounts.sendEnrollmentEmail = (userId, email, extraTokenData, extraParams) => {
 Meteor.methods({resetPassword: function (...args) {
   const token = args[0];
   const newPassword = args[1];
+  const extraOptions = args[2];
   return Accounts._loginMethod(
     this,
     "resetPassword",
@@ -713,6 +714,7 @@ Meteor.methods({resetPassword: function (...args) {
     () => {
       check(token, String);
       check(newPassword, passwordValidator);
+      check(extraOptions, Match.Maybe(Object));
 
       let user = Meteor.users.findOne(
         {"services.password.reset.token": token},
@@ -856,6 +858,7 @@ Accounts.sendVerificationEmail = (userId, email, extraTokenData, extraParams) =>
 // and log them in.
 Meteor.methods({verifyEmail: function (...args) {
   const token = args[0];
+  const extraOptions = args[1];
   return Accounts._loginMethod(
     this,
     "verifyEmail",
@@ -863,6 +866,7 @@ Meteor.methods({verifyEmail: function (...args) {
     "password",
     () => {
       check(token, String);
+      check(extraOptions, Match.Maybe(Object));
 
       const user = Meteor.users.findOne(
         {'services.email.verificationTokens.token': token},
@@ -1073,6 +1077,7 @@ const createUser = options => {
 // method for create user. Requests come from the client.
 Meteor.methods({createUser: function (...args) {
   const options = args[0];
+  const extraOptions = args[1];
   return Accounts._loginMethod(
     this,
     "createUser",
@@ -1081,6 +1086,8 @@ Meteor.methods({createUser: function (...args) {
     () => {
       // createUser() above does more checking.
       check(options, Object);
+      check(extraOptions, Match.Maybe(Object));
+
       if (Accounts._options.forbidClientAccountCreation)
         return {
           error: new Meteor.Error(403, "Signups forbidden")
