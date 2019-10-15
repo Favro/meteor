29b361b91b0bc48fe876798d3b8bea1b3edd3416
Oleksandr Chekhovskyi <oleksandr.chekhovskyi@favro.com>
2019-10-15 14:14:30 +0300

    Disable query snapshotting and diffing when applying server writes on the client
    This significantly reduces message processing overhead.

--- packages/ddp-client/common/livedata_connection.js
+++ packages/ddp-client/common/livedata_connection.js
@@ -1204,10 +1204,7 @@ export class Connection {
     if (self._resetStores || !isEmpty(updates)) {
       // Synchronous store updates for client
       Object.values(self._stores).forEach(store => {
-        store.beginUpdate(
-          updates[store._name]?.length || 0,
-          self._resetStores
-        );
+        store.beginUpdate(0, self._resetStores);
       });
 
       self._resetStores = false;
