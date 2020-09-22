0d4ccc011b2a31dffb521218a471a50e2f6c367c
Erik Olofsson <erik.olofsson@favro.com>
2020-09-22 09:17:31 +0200

    Enable CORS for sockjs

--- packages/ddp-server/transports/sockjs.js
+++ packages/ddp-server/transports/sockjs.js
@@ -27,9 +27,9 @@ export function createSockJSTransport() {
         // combining CPU-heavy processing with SockJS termination (eg a proxy which
         // converts to Unix sockets) but for now, raise the delay.
         disconnect_delay: 60 * 1000,
-        // Allow disabling of CORS requests to address
-        // https://github.com/meteor/meteor/issues/8317.
-        disable_cors: !!process.env.DISABLE_SOCKJS_CORS,
+        // CORS requests are disabled unless explicitly enabled with
+        // METEOR_ENABLE_CORS (see https://github.com/meteor/meteor/issues/8317).
+        disable_cors: !process.env.METEOR_ENABLE_CORS,
         // Set the USE_JSESSIONID environment variable to enable setting the
         // JSESSIONID cookie. This is useful for setting up proxies with
         // session affinity.
