f1ebaadfd19cc50027169a680ba8f50d4d56c0db
Erik Olofsson <erik.olofsson@favro.com>
2021-03-11 15:51:13 +0100

    Add hooks into OAuth machinery

--- packages/oauth1/oauth1_server.js
+++ packages/oauth1/oauth1_server.js
@@ -6,7 +6,7 @@ OAuth._queryParamsWithAuthTokenUrl = (authUrl, oauthBinding, params = {}, whitel
 
   Object.assign(
     redirectUrlObj.query,
-    whitelistedQueryParams.reduce((prev, param) => 
+    whitelistedQueryParams.reduce((prev, param) =>
       params.query[param] ? { ...prev, param: params.query[param] } : prev,
       {}
     ),
@@ -25,7 +25,10 @@ OAuth._queryParamsWithAuthTokenUrl = (authUrl, oauthBinding, params = {}, whitel
 };
 
 // connect middleware
-OAuth._requestHandlers['1'] = (service, query, res) => {
+OAuth._requestHandlers['1'] = (service, query, res, req) => {
+  if (OAuth.resultHandlerOverride(service, query, res, req))
+    return;
+
   const config = ServiceConfiguration.configurations.findOne({service: service.serviceName});
   if (! config) {
     throw new ServiceConfiguration.ConfigError(service.serviceName);
