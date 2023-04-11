7cf924a7df40151442150d8d33e6e96c591de2fd
matheusccastro <camargodecastromatheus@gmail.com>
2023-04-11 13:16:03 -0300

    Pass the `lastDisconnectError` and the outstanding methods names to the `_reconnectionHooks`

--- packages/ddp-client/common/livedata_connection.js
+++ packages/ddp-client/common/livedata_connection.js
@@ -1723,8 +1723,10 @@ export class Connection {
     self._outstandingMethodBlocks = [];
 
     self.onReconnect && self.onReconnect();
+
+    const oldOutstandingMethodNames = oldOutstandingMethodBlocks.flatMap(({ methods }) => methods).map(({ _message }) => _message.method);
     DDP._reconnectHook.each(callback => {
-      callback(self);
+      callback(self, oldOutstandingMethodNames, self.lastDisconnectError);
       return true;
     });
 
