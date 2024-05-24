c1722abbafd7693e41f8553442d60f013d0f1fa4
George Azzam <george.azzam@favro.com>
2024-05-24 18:39:50 +0200

    Deny HEAD requests with body

--- packages/webapp/webapp_server.js
+++ packages/webapp/webapp_server.js
@@ -1057,6 +1057,15 @@ async function runWebAppServer() {
 
   // webserver
   var app = createExpressApp()
+  app.use(function (req, res, next) {
+    if (req.method === 'HEAD' && req.headers['content-length'] > 0) {
+      res.writeHead(403);
+      res.write('Not allowed');
+      res.end();
+      return;
+    }
+    next();
+  });
 
   // Packages and apps can add handlers that run before any other Meteor
   // handlers via WebApp.rawExpressHandlers.
